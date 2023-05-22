65fa06c8790e7b08723496506de945fb8fc17076
hank <hank1234253@gmail.com>
2023-05-22 09:28:13 +0800

    以聯表查詢方式在結果列表中呈現各主題的總票數

--- front/result_list.php
+++ front/result_list.php
@@ -1,23 +1,25 @@
 <?php
-$id=$_GET['id'];
-$options=$pdo->query("select * from `options` where `subject_id`=$id")->fetchAll();
-$subject=$pdo->query("select * from `topics` where `id`=$id")->fetch();
+$subjects=$pdo->query("SELECT `topics`.`id`,
+                              `topics`.`subject`,
+                              SUM(`options`.`total`) as sum 
+                       FROM `options`, `topics` 
+                       WHERE `options`.`subject_id` = `topics`.`id` 
+                       GROUP BY `options`.`subject_id`")->fetchAll();
 ?>
-<h1>投票結果</h1>
-<h2><?=$subject['subject']?></h2>
+<h1>選擇你想要看的投票項目</h1>
 <ul class="topic-list">
-    <li class="list-row">
+    <li class="vote-option-title">
         <div class="vote-item">序號</div>
-        <div class="vote-item">項目</div>
+        <div class="vote-item">主題</div>
         <div class="vote-item">票數</div>
     </li>
     <?php
-    foreach($options as $idx =>$option){
+    foreach($subjects as $idx =>$subject){
     ?>
     <li class="list-row">
         <div class="vote-item"><?=$idx+1?>.</div>
-        <div class="vote-item"><?=$option['description']?></div>
-        <div class="vote-item"><?=$option['total']?></div>
+        <div class="vote-item"><a href="index.php?do=result&id=<?=$subject['id']?>"><?=$subject['subject']?></a></div>
+        <div class="vote-item"><?=$subject['sum']?></div>
     </li>
     <?php
     }
